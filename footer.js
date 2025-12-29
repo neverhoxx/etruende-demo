0dc0d595d0213cdd9d607e7a7270e5b7697f3475
yehor <yehor.kiiakh@gmail.com>
2025-12-29 20:22:11 +0200

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,5 @@
+
+
+const pageBody = document.body;
+
+body.innerHTML = footer;
---
+++ script.js
@@ -0,0 +1,43 @@
+const header = `
+<header>
+    <div class="container container-header">
+        <a href="">
+            <img src="./imgs/Group 11.svg" alt="">
+        </a>
+
+        <ul>
+            <li>
+                <a href="./index.html">
+                    MAIN
+                </a>
+            </li>
+            <li>
+                <a href="./gallery.html">
+                    GALLERY
+                </a>
+            </li>
+            <li>
+                <a href="./Projects.html">
+                    PROJECTS
+                </a>
+            </li>
+            <li>
+                <a href="./certifications.html">
+                    CERTIFICATIONS
+                </a>
+            </li>
+            <li>
+                <a href="./contacts.html">
+                    CONTACTS
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
+`
+
+
+const body = document.body;
+
+body.innerHTML = header;
+
